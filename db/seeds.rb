ba4aa907122b8c4065dc3173948cc3459e7a7cc3
G <mail@grahamirvine.com>
2023-03-28 11:02:24 -0600

    created code to auto seed flights

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,10 +8,31 @@
 
 Airport.delete_all
 
+Flight.delete_all
 
+# Seed airports
 Airport.create([{ airport_code: 'YYC'}, { airport_code: 'YEG'}, { airport_code: 'YFC'}, { airport_code: 'YQX'}, { airport_code: 'YHZ'}, { airport_code: 'YQM'}, { airport_code: 'YUL'}, { airport_code: 'YOW'}, { airport_code: 'YQB'}, { airport_code: 'YYT'}, { airport_code: 'YYZ'}, { airport_code: 'YVR'}, { airport_code: 'YWG'}])
 
+#Seed flights
 
-# Airport.create([{ airport_code: 'YYC'}, { airport_code: 'YEG'}, { airport_code: 'YFC'}, { airport_code: 'YQX'}, { airport_code: 'YHZ'}, { airport_code: 'YQM'}, { airport_code: 'YUL'}, { airport_code: 'YOW'}, { airport_code: 'YQB'}, { airport_code: 'YYT'}, { airport_code: 'YYZ'}, { airport_code: 'YVR'}, { airport_code: 'YWG'} ])
+Flight.delete_all
+
+airport_codes = Airport.pluck(:id)
+
+def time_rand
+  min_time = Time.now + 1.day
+  max_time = Time.now + 2.year
+  rand(min_time..max_time)
+end
+
+10.times do
+  dep_code = airport_codes.sample
+  arr_code = airport_codes.sample
+  until dep_code != arr_code
+    arr_code = airport_codes.sample
+  end
+
+  Flight.create(departure_airport_id: dep_code, arrival_airport_id: arr_code, departure_time: time_rand, flight_duration: rand(30..480))
+end
 
 
