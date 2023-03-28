672d2b92adaf1f2430cdcdde10c840ca33e021c4
G <mail@grahamirvine.com>
2023-03-28 15:00:53 -0600

    setup date picker to show only dates with available flights

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -4,4 +4,17 @@ class Flight < ApplicationRecord
 
   belongs_to :departure_airport, class_name: 'Airport'
   belongs_to :arrival_airport, class_name: 'Airport'
+
+
+  # def self.get_event_dates
+  #   event_dates = UgradRsvp.find_by_sql("select distinct event_date from admissions.ugrad_rsvps where event_date is not null order by event_date desc")
+  # end
+
+  def self.get_departure_dates
+    Flight.select(:departure_time).distinct
+  end
+
+  def departure_date_formatted
+    departure_time.strftime('%B %e, %Y')
+  end
 end
