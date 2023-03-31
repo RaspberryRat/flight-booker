1dc73f52446e4126c3b1bfdb05b1d5924a65f2e6
G <mail@grahamirvine.com>
2023-03-31 15:45:36 -0600

    updated assocaition in models to fix error

--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -2,5 +2,6 @@ class Passenger < ApplicationRecord
   validates :name, presence: true, length: { in: 3..15 }
   validates :email, presence: true, uniqueness: true
 
+  has_many :bookings
   has_many :flights, through: :bookings
 end
