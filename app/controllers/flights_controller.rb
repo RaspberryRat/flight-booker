0cd2563246b9bb78946947739a852a9017e87071
G <mail@grahamirvine.com>
2023-03-31 17:03:05 -0600

    updated params in controller

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,11 +1,11 @@
 class FlightsController < ApplicationController
   def index
-    @flights = Flight.find_flights(params)
+    @flights = Flight.find_flights(flight_params)
   end
 
   private
 
   def flight_params
-    params.require(:flight).permit(:departure_airport_id, :arrival_airport_id, :passenger_count, :departure_time)
+    params.permit(:departure_airport_id, :arrival_airport_id, :passenger_count, :departure_time)
   end
 end
