05b5967e4ad74daa4d553e391fe08b54ded239d4
G <mail@grahamirvine.com>
2023-04-02 17:13:21 -0600

    save state

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -10,7 +10,8 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+        sans: ['Helvetica', 'sans-serif'],
+
       },
     },
   },
