1973929975b737a319baccc4d4597eb38f25ac8f
G <mail@grahamirvine.com>
2023-04-02 17:25:57 -0600

    fixed bug with passenger model limiting email size

--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -1,6 +1,6 @@
 class Passenger < ApplicationRecord
   validates :name, presence: true, length: { in: 3..15 }
-  validates :email, presence: true, length: { in: 3..15 }
+  validates :email, presence: true
 
   belongs_to :booking
   has_many :flights, through: :bookings
