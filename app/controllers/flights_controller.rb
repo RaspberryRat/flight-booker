ab6a60bc901e3282b0962581961d94600f755c2b
G <mail@grahamirvine.com>
2023-04-03 10:34:02 -0600

    styling flight picker box

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,11 +1,18 @@
 class FlightsController < ApplicationController
   def index
     @flights = Flight.find_flights(flight_params)
+    @date = flight_search_date(@flight)
   end
 
   private
 
   def flight_params
     params.permit(:departure_airport_id, :arrival_airport_id, :passenger_count, :departure_time)
+  end
+
+  def flight_search_date(flight)
+    return nil if flight.blank?
+
+    flight.first.departure_date_human_readable_format
   end
 end
