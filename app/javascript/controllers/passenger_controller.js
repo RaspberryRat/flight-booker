f93e1ea3799ac5da76da27188bbe01f6bae18572
G <mail@grahamirvine.com>
2023-04-30 08:56:31 -0600

    add unique passenger fields on fly, passengers are not being added to new booking

--- app/javascript/controllers/passenger_controller.js
+++ app/javascript/controllers/passenger_controller.js
@@ -2,10 +2,7 @@ import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
   static targets = [ "passengerForm", "passengerList" ]
-
-  connect() {
-    //add number of passengers based on params
-  }
+  static values = { index: { type: Number, default: 0 } }
 
   addPassenger() {
     const clone = this.passengerListTarget.content.cloneNode(true);
@@ -14,13 +11,15 @@ export default class extends Controller {
     let labels = clone.querySelectorAll('label')
 
     labels[0].textContent = "Passenger Name:"
-    labels[0].setAttribute('for', "booking_passengers_attributes_0_name")
-    inputs[0].setAttribute('id', 'booking[passengers_attributes][0][name]')
-    inputs[0].setAttribute('name', 'booking_passengers_attributes_0_name')
+    labels[0].setAttribute('for', `booking_passengers_attributes_${this.indexValue}_name`)
+    inputs[0].setAttribute('id', `booking[passengers_attributes][${this.indexValue}][name]`)
+    inputs[0].setAttribute('name', `booking_passengers_attributes_${this.indexValue}_name`)
     labels[1].textContent = "Passenger Email:"
-    labels[1].setAttribute('for', "booking_passengers_attributes_0_email")
-    inputs[1].setAttribute('id', 'booking[passengers_attributes][0][email]')
-    inputs[1].setAttribute('name', 'booking_passengers_attributes_0_email')
+    labels[1].setAttribute('for', `booking_passengers_attributes_${this.indexValue}_email`)
+    inputs[1].setAttribute('id', `booking[passengers_attributes][${this.indexValue}][email]`)
+    inputs[1].setAttribute('name', `booking_passengers_attributes_${this.indexValue}_email`)
+
+    this.indexValue++
 
     this.passengerFormTarget.append(clone)
   }
